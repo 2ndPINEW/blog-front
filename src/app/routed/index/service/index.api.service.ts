045052699b2f27a4e3790b4eae242d79867eae48
高瀬英都 <>
2022-10-19 07:30:33 +0900

    トップページのリストのデータをキャッシュするように

--- src/app/routed/index/service/index.api.service.ts
+++ src/app/routed/index/service/index.api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { ApiService } from "src/app/shared/service/api.service";
 import { BlogListData } from "src/app/routed/index/service/index.interface";
-import { Observable } from "rxjs";
+import { Observable, of, tap } from "rxjs";
 
 @Injectable()
 export class IndexApiService {
@@ -9,7 +9,18 @@ export class IndexApiService {
     private api: ApiService
   ) {}
 
+  memoryCache: { data: BlogListData, page: number }[] = []
+
   getList (page: number): Observable<BlogListData> {
-    return this.api.get(`page-${page}.json`)
+    const cache = this.memoryCache.find(cache => cache.page === page)
+    if (cache) {
+      return of(cache.data)
+    }
+
+    return this.api.get<BlogListData>(`page-${page}.json`).pipe(
+      tap(data => {
+        this.memoryCache.push({ page, data })
+      })
+    )
   }
 }
