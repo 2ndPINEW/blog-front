823c8a8a7aa4779489fa3fe7d7a291e6fe4dea2c
高瀬英都 <>
2022-10-19 07:30:03 +0900

    ブログページ用のコンポーネント作成

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: '', loadChildren: () => import('./routed/index/index.module').then(m => m.IndexModule) }, { path: 'test', loadChildren: () => import('./routed/test/test.module').then(m => m.TestModule) }];
+const routes: Routes = [{ path: '', loadChildren: () => import('./routed/index/index.module').then(m => m.IndexModule) }, { path: 'test', loadChildren: () => import('./routed/test/test.module').then(m => m.TestModule) }, { path: 'blog', loadChildren: () => import('./routed/blog/blog.module').then(m => m.BlogModule) }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
