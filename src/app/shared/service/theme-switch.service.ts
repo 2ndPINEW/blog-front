964727b4369345d2d6436406f37d2ef0312ecfa8
Eito Takase <eito01178@gmail.com>
2022-11-02 22:09:37 +0900

    画像を切り替える処理を一箇所にまとめる準備

--- src/app/shared/service/theme-switch.service.ts
+++ src/app/shared/service/theme-switch.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 
-export type Theme = 'none' | 'halloween'
+export type Theme = 'none' | 'halloween' | 'xmas'
 
 @Injectable({
   providedIn: 'root'
@@ -18,6 +18,51 @@ export class ThemeSwitchService {
     if (isHalloween) {
       this.theme = 'halloween'
       document.body.classList.add('halloween')
+    }
+
+    const isXmas = true //today.getMonth() === 11 && 18 <= today.getDate() && today.getDate() <= 25
+    if (isXmas) {
+      this.theme = 'xmas'
+      document.body.classList.add('xmas')
+    }
+  }
+
+  /** ヘッダーのアイコン */
+  get headerIcon (): string | undefined {
+    switch (this.theme) {
+      case 'halloween':
+        return '/assets/images/halloween/halloween_text_e.png'
+      case 'xmas':
+        return `/assets/images/xmas/logo.png`
+      default:
+        return undefined
+    }
+  }
+
+  /** 記事カードのサムネイル */
+  get blogCardThumbnail (): string | undefined {
+    switch (this.theme) {
+      case 'halloween':
+        return '/assets/images/halloween/halloween_mark_majo.png'
+      case 'xmas':
+        const index = Math.floor(Math.random() * 3) + 1
+        return `/assets/images/xmas/icons/${index}.png`
+      default:
+        return undefined
+    }
+  }
+
+  /** 目次の読み終わったマーク */
+  get tocCompleteMark (): string {
+    switch (this.theme) {
+      case 'none':
+        return '/assets/images/service/logo_s.png'
+      case 'halloween':
+        return '/assets/images/halloween/halloween_mark_majo.png'
+      case 'xmas':
+        return '/assets/images/xmas/icons/2.png'
+      default:
+        return '/assets/images/service/logo_s.png'
     }
   }
 }
