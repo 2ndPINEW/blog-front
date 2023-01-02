3bbc4156725359b2c807f66b54af8226c303db98
Eito Takase <eito01178@gmail.com>
2023-01-03 00:38:51 +0900

    🚑ライセンスページのタイトルが変わるように

--- src/app/routed/licenses/licenses.component.ts
+++ src/app/routed/licenses/licenses.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { SeoService } from 'src/app/shared/service/seo.service';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -12,10 +13,12 @@ export class LicensesComponent implements OnInit {
   license$!: Observable<string>
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private seo: SeoService
   ) { }
 
   ngOnInit(): void {
+    this.seo.update('ライセンス', '3rdpartylicense')
     if (environment.production) {
       this.license$ = this.http.get('/3rdpartylicenses.txt', { responseType: 'text' })
     } else {
