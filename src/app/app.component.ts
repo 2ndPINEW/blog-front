0532678d0033ccd2be965abfc808620564b6c88d
Eito Takase <eito01178@gmail.com>
2023-01-19 20:49:25 +0900

    🚑たまに操作できないことがありそうなので修正

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { fromEvent, race, timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +16,11 @@ export class AppComponent implements OnInit {
     if (!loadingScreen) {
       return
     }
-    loadingScreen.addEventListener('transitionend', () => {
+    // transitionendが発火しないことがたまにありそうなので、適当にタイムアウトする
+    race(
+      fromEvent(loadingScreen, 'transitionend'),
+      timer(500)
+    ).subscribe(() => {
       loadingScreen.remove()
     })
     loadingScreen.classList.add('loaded')
