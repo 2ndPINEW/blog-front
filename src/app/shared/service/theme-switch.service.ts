7aff8fa93b540f511fb8ff74b274844d9961ac59
Eito Takase <eito01178@gmail.com>
2023-05-03 14:45:48 +0900

    ♻️format

--- src/app/shared/service/theme-switch.service.ts
+++ src/app/shared/service/theme-switch.service.ts
@@ -1,112 +1,127 @@
 import { Injectable } from "@angular/core";
 
 export interface ThemeConfig {
-  theme: Theme
-  label: string
-  condition: (today: Date) => boolean
+  theme: Theme;
+  label: string;
+  condition: (today: Date) => boolean;
 }
 
-export type Theme = 'none' | 'halloween' | 'xmas' | 'spring' | 'valentine'
+export type Theme = "none" | "halloween" | "xmas" | "spring" | "valentine";
 
-export const ThemeConfigs: ThemeConfig[] = [{
-  theme: 'none',
-  label: 'リセット(自動)',
-  condition: (_: Date) => {
-    return false
-  }
-}, {
-  theme: 'halloween',
-  label: 'ハロウィン',
-  condition: (today: Date) => {
-    return today.getDate() >= 24 && today.getMonth() === 9
-  }
-}, {
-  theme: 'xmas',
-  label: 'クリスマス',
-  condition: (today: Date) => {
-    return today.getMonth() === 11 && 18 <= today.getDate() && today.getDate() <= 25
-  }
-}, {
-  theme: 'spring',
-  label: '春',
-  condition: (today: Date) => {
-    return today.getMonth() === 3
-  }
-}, {
-  theme: 'valentine',
-  label: 'バレンタイン',
-  condition: (today: Date) => {
-    return today.getMonth() === 1 && today.getDate() === 14
-  }
-}]
+export const ThemeConfigs: ThemeConfig[] = [
+  {
+    theme: "none",
+    label: "リセット(自動)",
+    condition: (_: Date) => {
+      return false;
+    },
+  },
+  {
+    theme: "halloween",
+    label: "ハロウィン",
+    condition: (today: Date) => {
+      return today.getDate() >= 24 && today.getMonth() === 9;
+    },
+  },
+  {
+    theme: "xmas",
+    label: "クリスマス",
+    condition: (today: Date) => {
+      return (
+        today.getMonth() === 11 &&
+        18 <= today.getDate() &&
+        today.getDate() <= 25
+      );
+    },
+  },
+  {
+    theme: "spring",
+    label: "春",
+    condition: (today: Date) => {
+      return today.getMonth() === 3;
+    },
+  },
+  {
+    theme: "valentine",
+    label: "バレンタイン",
+    condition: (today: Date) => {
+      return today.getMonth() === 1 && today.getDate() === 14;
+    },
+  },
+];
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ThemeSwitchService {
-  theme: Theme = 'none'
+  theme: Theme = "none";
 
-  constructor () {}
+  constructor() {}
 
-  init (): void {
-    const today = new Date()
-    const forceUseTheme = sessionStorage.getItem('theme')
+  init(): void {
+    const today = new Date();
+    const forceUseTheme = sessionStorage.getItem("theme");
 
-    document.body.className = ''
-    this.theme = 'none'
+    document.body.className = "";
+    this.theme = "none";
 
-    const themeConfig = ThemeConfigs.find(themeConfig => themeConfig.condition(today) || themeConfig.theme === forceUseTheme)
+    const themeConfig = ThemeConfigs.find(
+      (themeConfig) =>
+        themeConfig.condition(today) || themeConfig.theme === forceUseTheme
+    );
     if (themeConfig) {
-      this.theme = themeConfig.theme
-      document.body.classList.add(themeConfig.theme)
+      this.theme = themeConfig.theme;
+      document.body.classList.add(themeConfig.theme);
     }
   }
 
-  setDefaultTheme (theme: Theme): void {
-    window.sessionStorage.setItem('theme', theme)
-    this.init()
+  setDefaultTheme(theme: Theme): void {
+    window.sessionStorage.setItem("theme", theme);
+    this.init();
   }
 
   /** ヘッダーのアイコン */
-  get headerIcon (): string | undefined {
+  get headerIcon(): string | undefined {
     switch (this.theme) {
-      case 'halloween':
-        return '/assets/images/halloween/halloween_text_e.png'
-      case 'xmas':
-        return `/assets/images/xmas/logo.png`
+      case "halloween":
+        return "/assets/images/halloween/halloween_text_e.png";
+      case "xmas":
+        return `/assets/images/xmas/logo.png`;
       default:
-        return undefined
+        return undefined;
     }
   }
 
   /** 記事カードのサムネイル */
-  get blogCardThumbnail (): string | undefined {
+  get blogCardThumbnail(): string | undefined {
     switch (this.theme) {
-      case 'halloween':
-        return '/assets/images/service/logo_s.png'
-      case 'xmas':
-        const index = Math.floor(Math.random() * 3) + 1
-        return `/assets/images/xmas/icons/${index}.png`
+      case "halloween":
+        return "/assets/images/service/logo_s.png";
+      case "xmas":
+        const index = Math.floor(Math.random() * 3) + 1;
+        return `/assets/images/xmas/icons/${index}.png`;
       default:
-        return undefined
+        return undefined;
     }
   }
 
   /** 目次の読み終わったマーク */
-  get tocCompleteMark (): string {
+  get tocCompleteMark(): string {
     switch (this.theme) {
-      case 'none':
-        return '/assets/images/service/logo_s.png'
-      case 'halloween':
-        return '/assets/images/halloween/halloween_mark_majo.png'
-      case 'xmas':
-        return '/assets/images/xmas/icons/2.png'
+      case "none":
+        return "/assets/images/service/logo_s.png";
+      case "halloween":
+        return "/assets/images/halloween/halloween_mark_majo.png";
+      case "xmas":
+        return "/assets/images/xmas/icons/2.png";
       default:
-        return '/assets/images/service/logo_s.png'
+        return "/assets/images/service/logo_s.png";
     }
   }
 }
 
-export function ThemeSwitchServiceInit (themeSwitchService: ThemeSwitchService): Function {
-  return () => themeSwitchService.init()
+export function ThemeSwitchServiceInit(
+  themeSwitchService: ThemeSwitchService
+): Function {
+  return () => themeSwitchService.init();
 }
