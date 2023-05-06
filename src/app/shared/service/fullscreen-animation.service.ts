35d55a2269f831bb3e540ca6f28bf5e7e3654126
Eito Takase <eito01178@gmail.com>
2023-05-06 13:44:48 +0900

    confetti

--- src/app/shared/service/fullscreen-animation.service.ts
+++ src/app/shared/service/fullscreen-animation.service.ts
@@ -1,27 +1,78 @@
 import { Injectable } from "@angular/core";
 import { NavigationEnd, Router } from "@angular/router";
+import confetti from "canvas-confetti";
+import { take, timer } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class FullscreenAnimationService {
-  isAnimationEnable: boolean = true
+  isAnimationEnable: boolean = true;
 
-  constructor (
-    private router: Router
-  ) {
-    this.router.events.subscribe(event => {
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
-        this.enableAnimation()
+        this.enableAnimation();
       }
-    })
+    });
   }
 
-  enableAnimation (): void {
-    this.isAnimationEnable = true
+  enableAnimation(): void {
+    this.isAnimationEnable = true;
   }
 
-  disableAnimation (): void {
-    this.isAnimationEnable = false
+  disableAnimation(): void {
+    this.isAnimationEnable = false;
+  }
+
+  confetti() {
+    confetti({
+      particleCount: 200,
+      spread: 120,
+      angle: 60,
+      origin: { x: 0, y: 1 },
+      scalar: 1.5,
+    });
+    confetti({
+      particleCount: 200,
+      spread: 120,
+      angle: 120,
+      origin: { x: 1, y: 1 },
+      scalar: 1.5,
+    });
+  }
+
+  stars() {
+    timer(400, 200)
+      .pipe(take(4))
+      .subscribe(() => {
+        this.star(Math.random(), Math.random());
+      });
+  }
+
+  star(x: number, y: number) {
+    const defaults = {
+      spread: 360, // 360度 ± 22.5度に出現
+      ticks: 50,
+      gravity: 0,
+      startVelocity: 30,
+      shapes: ["star"],
+      origin: { x, y },
+      colors: ["FFE400", "FFBD00", "E89400", "FFCA6C", "FDFFB8"],
+    };
+
+    confetti({
+      ...defaults,
+      particleCount: 100,
+      scalar: 1.2,
+      shapes: ["star"],
+    });
+
+    confetti({
+      ...defaults,
+      particleCount: 100,
+      scalar: 0.75,
+      shapes: ["circle"],
+    });
   }
 }
